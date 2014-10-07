588df482f35974f72f14747b0547edff189f7f0f
Jared Parsons <jaredpar@microsoft.com>
2014-10-07 15:38:25 -0700

    Basic project structure

---
+++ Src/NewEnumerable/IEnumerable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEnumerable
+{
+    public interface IEnumerable<TElement, TIterator>
+    {
+        TIterator Start { get; }
+
+        bool TryGetNext(ref TIterator current, out TElement value);
+    }
+}
