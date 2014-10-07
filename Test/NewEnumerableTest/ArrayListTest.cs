a600fad18c071874baaf2a96611e3549db7bdd1e
Jared Parsons <jaredpar@microsoft.com>
2014-10-07 16:00:48 -0700

    Simple unit test

---
+++ Test/NewEnumerableTest/ArrayListTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NewEnumerable.UnitTest
+{
+    public sealed class ArrayListTest
+    {
+        private void TestSimple<T>(params T[] p)
+        {
+            var arrayList = new ArrayList<T>();
+            for (int i = 0; i < p.Length; i++)
+            {
+                arrayList.Add(p[i]);
+            }
+
+            var e = arrayList.Start;
+            for (int i = 0; i < p.Length; i++)
+            {
+                T value;
+                Assert.True(arrayList.TryGetNext(ref e, out value));
+                Assert.Equal(p[i], value);
+            }
+
+            T unused;
+            Assert.False(arrayList.TryGetNext(ref e, out unused));
+        }
+
+        [Fact]
+        public void SimpleEnumeration()
+        {
+            TestSimple("cat", "dog");
+            TestSimple(1, 2, 45, 4);
+        }
+    }
+}
