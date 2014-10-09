54e8ec8215b8973c0e3f8418f82255106aea82f0
Jared Parsons <jaredpar@microsoft.com>
2014-10-09 13:45:58 -0700

    Runner
    Simple test runner project.  Just lets me F5 various things for samples

---
+++ Test/Runner/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEnumerable.Runner
+{
+    internal sealed class Program
+    {
+
+        private static void PrintAll(IEnumerable<string, int> e)
+        {
+            int index = e.Start;
+            string value;
+            while (e.TryGetNext(ref index, out value))
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+
+
+        internal static void Main(string[] args)
+        {
+            var arrayList = new ArrayList<string>();
+            arrayList.Add("cat");
+            arrayList.Add("dog");
+            arrayList.Add("fish");
+            PrintAll(arrayList);
+            PrintAll(arrayList);
+        }
+    }
+}
